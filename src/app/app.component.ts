3d299fe4584f0984d1016fa4f97510c3d7574812
alibsg <30772100+alibsg@users.noreply.github.com>
2024-08-24 22:57:20 +0300

    showing from value from dynamic forms and how to control form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { JsonPipe } from '@angular/common';
 
-import { DynamicFormComponent } from 'dynamic-form-library';
+import { DynamicFormComponent, DynamicFormService } from 'dynamic-form-library';
 import { formFields } from './form-fields';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, DynamicFormComponent],
+  imports: [RouterOutlet, DynamicFormComponent, JsonPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   fields = formFields;
+  dynamicFormService = inject(DynamicFormService);
+  formGroup = this.dynamicFormService.createForm(this.fields);
+  formValue: any;
+
+  onSubmit() {
+    this.formValue = this.formGroup.getRawValue();
+  }
+
+  onClear() {
+    this.formGroup.reset();
+    this.onSubmit();
+  }
 }
